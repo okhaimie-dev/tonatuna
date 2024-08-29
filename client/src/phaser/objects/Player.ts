9931ff29b4ec4136740ad148a62c13d8ab1404a6
Boffee <zhenbangli@gmail.com>
2024-08-28 21:34:32 -0400

    fix: player spawn coordinate

--- client/src/phaser/objects/Player.ts
+++ client/src/phaser/objects/Player.ts
@@ -12,8 +12,8 @@ export class Player extends Phaser.GameObjects.Container {
     y: number,
     key: string
   ) {
-    const surfSprite = scene.add.sprite(0, 0, "surf");
-    const playerSprite = scene.add.sprite(4, -8, "surf");
+    const surfSprite = scene.add.sprite(x * 32, y * 32, "surf");
+    const playerSprite = scene.add.sprite(x * 32 + 4, y * 32 - 8, "surf");
 
     super(scene, x, y, [surfSprite, playerSprite]);
 
