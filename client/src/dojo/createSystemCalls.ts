ce2e443be9d2d51fd154d2035ed9e169c215ce94
Tony Stark <oktonystark8@gmail.com>
2024-08-29 18:29:31 -0400

    feat: wip stage

--- client/src/dojo/createSystemCalls.ts
+++ client/src/dojo/createSystemCalls.ts
@@ -2,6 +2,7 @@ import { Account } from "starknet";
 import { ClientComponents } from "./createClientComponents";
 import { getEvents, setComponentsFromEvents } from "@dojoengine/utils";
 import type { IWorld } from "./typescript/contracts.gen";
+import { Vec2 } from "./typescript/models.gen";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
@@ -31,7 +32,193 @@ export function createSystemCalls(
     }
   };
 
+  const new_player = async (account: Account, name: bigint) => {
+    try {
+      const { transaction_hash } = await client.actions.new_player({
+        account,
+        name,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+
+      // Wait for the indexer to update the entity
+      // By doing this we keep the optimistic UI in sync with the actual state
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const move = async (account: Account, dest_pos: Vec2) => {
+    try {
+      const { transaction_hash } = await client.actions.move({
+        account,
+        dest_pos,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+
+      // Wait for the indexer to update the entity
+      // By doing this we keep the optimistic UI in sync with the actual state
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const spawn_fish = async (
+    account: Account,
+    fish_pond_id: number,
+    fish_id: number,
+  ) => {
+    try {
+      const { transaction_hash } = await client.actions.spawn_fish({
+        account,
+        fish_pond_id,
+        fish_id,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const spawn_multiple_fishes = async (
+    account: Account,
+    fish_pond_id: number,
+    num_fish: number,
+  ) => {
+    try {
+      const { transaction_hash } = await client.actions.spawn_multiple_fishes({
+        account,
+        fish_pond_id,
+        num_fish,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const cast_fishing = async (
+    account: Account,
+    fish_pond_id: number,
+    commitment: bigint,
+  ) => {
+    try {
+      const { transaction_hash } = await client.actions.cast_fishing({
+        account,
+        fish_pond_id,
+        commitment,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const reel_by_revealing = async (
+    account: Account,
+    player_id: bigint,
+    fish_pond_id: number,
+    fish_id: number,
+    salt: number,
+  ) => {
+    try {
+      const { transaction_hash } = await client.actions.reel_by_revealing({
+        account,
+        player_id,
+        fish_pond_id,
+        fish_id,
+        salt,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const catch_the_fish = async (
+    account: Account,
+    player_id: bigint,
+    fish_pond_id: number,
+    fish_id: number,
+    salt: number,
+  ) => {
+    try {
+      const { transaction_hash } = await client.actions.catch_the_fish({
+        account,
+        player_id,
+        fish_pond_id,
+        fish_id,
+        salt,
+      });
+
+      setComponentsFromEvents(
+        contractComponents,
+        getEvents(
+          await account.waitForTransaction(transaction_hash, {
+            retryInterval: 100,
+          }),
+        ),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return {
     create_fish_pond,
+    new_player,
+    move,
+    spawn_fish,
+    spawn_multiple_fishes,
+    cast_fishing,
+    reel_by_revealing,
+    catch_the_fish,
   };
 }
