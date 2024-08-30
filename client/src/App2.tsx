85bab91db181b4985fb9d79a8d7faf0e15284c2b
Boffee <zhenbangli@gmail.com>
2024-08-29 22:19:29 -0400

    feat: template for recs to phaser

--- client/src/App2.tsx
+++ client/src/App2.tsx
@@ -1,9 +1,6 @@
 import PlayCard from "./components/PlayCard";
-import useRecsToPhaser from "./hooks/useRecsToPhaser";
 
 export default function App2() {
-  useRecsToPhaser();
-
   return (
     <div className="absolute left-0 top-0 flex flex-col h-full w-full pointer-events-none">
       <div className="flex flex-col items-center justify-center h-full">
